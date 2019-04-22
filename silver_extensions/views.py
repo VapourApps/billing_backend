dc668c14e3bb8cb10bced7c5e18d581bc7186f17
Mihail <m.nacev@yahoo.com>
2019-04-22 14:14:06 +0000

    Return plan_product code object in silver_extensions/views.py (get_plans view)

--- silver_extensions/views.py
+++ silver_extensions/views.py
@@ -38,6 +38,7 @@ def get_plans(request):
         plan_result = model_to_dict(plan)
         plan_result['plan_provider'] = model_to_dict(plan.provider) 
         plan_result['metered_features'] = [metered_feature_to_dict(x) for x in plan.metered_features.all()]
+        plan_result['product_code'] = model_to_dict(plan.product_code)
         if plan_features:
             plan_feature = plan_features[0]
             feature = model_to_dict(plan_feature)
