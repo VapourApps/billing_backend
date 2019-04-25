0646e8de06feacb8d34c5bdd56d4efb5a5b716c8
Mihail <m.nacev@yahoo.com>
2019-04-25 09:05:18 +0000

    Fix import error in silver_extensions/views.py

--- silver_extensions/views.py
+++ silver_extensions/views.py
@@ -64,7 +64,7 @@ def add_new_billing_log(request):
     d = date.today()
     b = BillingLog(subscription = sub, billing_date = d, metered_features_billed_up_to = d, plan_billed_up_to = d)
     if data.get('proforma_id'):
-        proforma = Proforma.objects.get(pk = data['proforma_id'])
+        proforma = s.Proforma.objects.get(pk = data['proforma_id'])
         b.proforma = proforma
     b.save()
     return JsonResponse({"success" : True})
