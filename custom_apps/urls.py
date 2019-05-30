1e4229176e44a478eb15a9a025b8fc7d027ae875
NinoDoko <ninodokoskiot@hotmail.com>
2019-05-30 12:03:34 +0000

    Updated initial data to fit new models.

--- custom_apps/urls.py
+++ custom_apps/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls import url, include
 import importlib
 
 app_names = get_app_names()
-print (app_names)
 urlpatterns = [url('^%s/' % app, include('custom_apps.%s.urls' % app)) for app in app_names]
 
